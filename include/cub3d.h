771c272ece0838a6866a36ee734e9dbd7e8b9c2a
samreerink <samreerink@gmail.com>
2024-11-21 00:40:57 +0100

    Header file

---
+++ include/cub3d.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                       ::::::::             */
+/*   cub3d.h                                           :+:    :+:             */
+/*                                                    +:+                     */
+/*   By: sreerink <sreerink@student.codam.nl>        +#+                      */
+/*                                                  +#+                       */
+/*   Created: 2024/11/19 15:00:08 by sreerink      #+#    #+#                 */
+/*   Updated: 2024/11/19 15:00:10 by sreerink      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include "../MLX42/include/MLX42/MLX42.h"
+# include "../Libft/libft.h"
+
+#endif
