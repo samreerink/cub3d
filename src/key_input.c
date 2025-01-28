f183b8f61c16d4361276a938b928bafd67c7c861
samreerink <samreerink@gmail.com>
2025-01-28 02:34:17 +0100

    Implemented (for now chunky) controls

--- src/key_input.c
+++ src/key_input.c
@@ -6,7 +6,7 @@
 /*   By: sreerink <sreerink@student.codam.nl>        +#+                      */
 /*                                                  +#+                       */
 /*   Created: 2025/01/23 17:09:06 by sreerink      #+#    #+#                 */
-/*   Updated: 2025/01/27 20:41:18 by sreerink      ########   odam.nl         */
+/*   Updated: 2025/01/28 02:27:50 by sreerink      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,51 @@
 void	key_input(mlx_key_data_t keydata, void *ptr)
 {
 	t_cube		*cube;
+	t_player	*p;
+	char		**m;
+	double		frame_time;
+	double		move_speed;
+	double		rot_speed;
 
 	cube = (t_cube *)ptr;
+	p = cube->player;
+	m = cube->map;
+	frame_time = cube->mlx->delta_time;
+	move_speed = frame_time * 2.5;
+	rot_speed = frame_time * 1.5;
 	if (keydata.key == MLX_KEY_ESCAPE && keydata.action == MLX_PRESS)
 		exit_cube(cube);
+	// Keys A and D are temp, need to study the code
+	if (keydata.key == MLX_KEY_W && keydata.action == MLX_REPEAT)
+	{
+		if (m[(int)p->pos_y][(int)(p->pos_x + p->dir_x * move_speed)] != '1')
+			p->pos_x += p->dir_x * move_speed;
+		if (m[(int)(p->pos_y + p->dir_y * move_speed)][(int)p->pos_x] != '1')
+			p->pos_y += p->dir_y * move_speed;
+	}
+	if (keydata.key == MLX_KEY_S && keydata.action == MLX_REPEAT)
+	{
+		if (m[(int)p->pos_y][(int)(p->pos_x - p->dir_x * move_speed)] != '1')
+			p->pos_x -= p->dir_x * move_speed;
+		if (m[(int)(p->pos_y - p->dir_y * move_speed)][(int)p->pos_x] != '1')
+			p->pos_y -= p->dir_y * move_speed;
+	}
+	if (keydata.key == MLX_KEY_A && keydata.action == MLX_REPEAT)
+	{
+		double oldDirX = p->dir_x;
+		p->dir_x = p->dir_x * cos(rot_speed) - p->dir_y * sin(rot_speed);
+		p->dir_y = oldDirX * sin(rot_speed) + p->dir_y * cos(rot_speed);
+		double oldPlaneX = p->plane_x;
+		p->plane_x = p->plane_x * cos(rot_speed) - p->plane_y * sin(rot_speed);
+		p->plane_y = oldPlaneX * sin(rot_speed) + p->plane_y * cos(rot_speed);
+	}
+	if (keydata.key == MLX_KEY_D && keydata.action == MLX_REPEAT)
+	{
+		double oldDirX = p->dir_x;
+		p->dir_x = p->dir_x * cos(-rot_speed) - p->dir_y * sin(-rot_speed);
+		p->dir_y = oldDirX * sin(-rot_speed) + p->dir_y * cos(-rot_speed);
+		double oldPlaneX = p->plane_x;
+		p->plane_x = p->plane_x * cos(-rot_speed) - p->plane_y * sin(-rot_speed);
+		p->plane_y = oldPlaneX * sin(-rot_speed) + p->plane_y * cos(-rot_speed);
+	}
 }
